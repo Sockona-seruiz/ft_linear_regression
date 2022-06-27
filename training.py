f76b0133c36e767d9c60d8c076fe4c938133e24d
seruiz <s.ruiz.sin@gmail.com>
2022-06-27 15:19:21 +0200

    remove useless price scaling

--- training.py
+++ training.py
@@ -1,60 +1,108 @@
 import numpy as np
+import math 
 import matplotlib.pyplot as plt
 
 prices = []
+d_prices = []
 km = []
-teta0 = 0
-teta1 = 0
-
-tmpteta0 = 0
-tmpteta1 = 0
+d_km = []
+iteration = 1000
 
 Lr = 0.1
+def read_data():
+  outfile = open("data.csv","r")
+  data = outfile.readlines()
+  # File parsing arranged int two arrays
+  for line in data:
+    if 'km' not in line:
+        words = line.split(",")
+        words[1] =words[1][:-1]
+        km.append(float(words[0]))
+        prices.append(float(words[1]))
+  outfile.close()
+  return (km, prices)
 
-outfile = open("data2.csv","r")
-data = outfile.readlines()
-# File parsing arranged int two arrays
-for line in data:
-  if 'km' not in line:
-      words = line.split(",")
-      words[1] =words[1][:-1]
-      km.append(float(words[0]))
-      prices.append(float(words[1]))
-      print("km = " + words[0] + "    price = " + words[1])
-outfile.close()
-
-j = 0
-LR_m = Lr/len(km)
-
-while j < 100:
-  sum0 = 0
-  sum1 = 0
-
+def scale_down(km):
+  a_max = max(km)
   i = 0
+  d_km = []
   while i < len(km):
-    sum0 += (teta0 + teta1 * km[i]) - prices[i]
-    sum1 += ((teta0 + teta1 * km[i]) - prices[i]) * km[i]
+    d_km.append(km[i] / a_max)
+    print("d_km = " + str(d_km[i]) + "    km = " + str(km[i]) )
     i += 1
+  return (d_km, a_max)
 
-  tmpteta0 = LR_m * sum0
-  tmpteta1 = LR_m * sum1
+def training(km, prices, iteration):
+  LR_m = Lr/len(km)
+  teta0 = 0
+  teta1 = 0
 
-  teta0 -= tmpteta0
-  teta1 -= tmpteta1
-  
-  j += 1
+  j = 0
+  while j < iteration:
+    sum0 = 0
+    sum1 = 0
+
+    i = 0
+    while i < len(km):
+      sum0 += (teta0 + teta1 * km[i]) - prices[i]
+      sum1 += ((teta0 + teta1 * km[i]) - prices[i]) * km[i]
+      i += 1
+
+    tmpteta0 = LR_m * sum0
+    tmpteta1 = LR_m * sum1
+
+    teta0 -= tmpteta0
+    teta1 -= tmpteta1
+
+    j += 1
+  return (teta0, teta1)
+
+[km, prices] = read_data()
+
+a_max = 0
+[d_km, a_max] = scale_down(km)
+
+# [teta0, teta1] = training(d_km, d_prices, iteration)
+[teta0, teta1] = training(d_km, prices, iteration)
+
+def mean_calc(km):
+  i = 0
+  sum = 0
+  while (i < len(km)):
+    sum += km[i]
+    i += 1
+  mean = sum / i
+  return (mean)
+
+def std_calc(mean, km):
+  i = 0
+  std = 0
+  while (i < len(km)):
+    std += pow((km[i] - mean), 2)
+    i += 1
+  std = std / i
+  std = math.sqrt(std)
+  return (std)
+
+ukm = mean_calc(km)
+
+std = std_calc(ukm, km)
+
+# teta0 = teta0 - (ukm/std) * teta1
+teta1 = teta1 / std
 
 
-# print("tmpteta0 = " + str(tmpteta0))
-# print("tmpteta1 = " + str(tmpteta1))
-print("teta0 = " + str(teta0))
-print("teta1 = " + str(teta1))
 
-x = np.linspace(0,2,2)
+print("teta0 = " + str(teta0) + "       teta1 = " + str(teta1))
+# teta0 *= max(prices)
+# teta1 *= max(km)
+# print("teta0 = " + str(teta0) + "       teta1 = " + str(teta1))
+
+x = np.linspace(0, 20000, 20000)
 y = teta0 + teta1 * x
 plt.plot(x, y, '-r', label='price = teta0 + teta1 * km')
 
 plt.plot(km, prices, 'ro')
 plt.ylabel('price')
 plt.xlabel('km')
-plt.show()   
+plt.show()
